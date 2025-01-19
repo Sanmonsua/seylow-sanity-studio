70748f0f53c32b8207d1589891b01467c0a28d18
Santiago Montoya <sanmonsua@gmail.com>
2025-01-19 17:36:07 -0500

    add schema types

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,1 +1,116 @@
-export const schemaTypes = []
+import {defineField, defineType} from 'sanity'
+
+const releaseType = defineType({
+  name: 'release',
+  title: 'Release',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'status',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'songlink',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'cover',
+      type: 'image',
+    }),
+  ],
+})
+
+const bannerType = defineType({
+  name: 'banner',
+  title: 'Banner',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'subtitle',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'link',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'callToAction',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'image',
+      type: 'image',
+    }),
+  ],
+})
+
+const profileInfoType = defineType({
+  name: 'profileInfo',
+  title: 'Profile Info',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'email',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'logo',
+      type: 'image',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'bio',
+      type: 'text',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'image',
+      type: 'image',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'backgroundImage',
+      type: 'image',
+    }),
+    defineField({
+      name: 'location',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+  ],
+})
+
+const listenNowContentType = defineType({
+  name: 'listenNowContent',
+  title: 'Listen Now Content',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'iframe',
+      type: 'text',
+    })
+  ],
+})
+
+export const schemaTypes = [releaseType, bannerType, profileInfoType, listenNowContentType]
